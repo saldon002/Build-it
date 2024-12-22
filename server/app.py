9ed1bab9660cd065a37704ee60cc1238ed159127
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2024-12-22 21:20:40 +0100

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    # Servi il file index.html
+    return render_template('index.html')
+
+@app.route('/configuration')
+def configuration():
+    # Servi la pagina dinamica configuration.html
+    return render_template('configuration.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
