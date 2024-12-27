626de09be62fdfe8c05b4869b16979ab52bcca85
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2024-12-27 19:48:22 +0100

    Delete models.py

--- server/models.py
+++
@@ -1,7 +0,0 @@
-from app import mongo
-
-def get_all_components():
-    return mongo.db.components.find()
-
-def get_all_component_by_id(component_id):
-    return mongo.db.components.find_one({"_id": component_id})
