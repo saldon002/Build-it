5f5b30a0c81662432c3fd8c96795f572dc8d593d
saldon002 <salvatore.donofrio002@studenti.uniparthenope.it>
2024-12-27 19:36:47 +0100

    Update config.py

--- server/config.py
+++ server/config.py
@@ -1,6 +1,11 @@
 import os
 
+
 class Config:
-    # Take ambient var for MongoDB
+    """Configuration class for Flask app settings"""
+
+    # Retrieve MongoDB URI from environment variable
+    # The URI is used to connect to the MongoDB database
     MONGO_URI = os.getenv('MONGO_URI', 'mongodb://mongodb:27017/mydatabase')
+    # The SECRET_KEY is used by Flask for session management
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
